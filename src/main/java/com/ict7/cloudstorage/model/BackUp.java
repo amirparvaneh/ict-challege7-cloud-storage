2316ad72c71988d659499002608b65513d343c68
amirparvaneh <amirparvaneh138@gmail.com>
2023-06-11 23:19:53 +0330

    dev-first

---
+++ src/main/java/com/ict7/cloudstorage/model/BackUp.java
@@ -0,0 +1,9 @@
+package com.ict7.cloudstorage.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "backUp")
+public class BackUp {
+}
---
+++ src/main/java/com/ict7/cloudstorage/model/Folder.java
@@ -0,0 +1,10 @@
+package com.ict7.cloudstorage.model;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "folder")
+public class Folder {
+}
